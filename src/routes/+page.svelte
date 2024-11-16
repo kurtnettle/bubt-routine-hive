417e7d6aa2c506576b1b95a49759103ab5c89d49
kurtnettle <89929240+KurtNettle@users.noreply.github.com>
2024-11-16 17:09:55 +0600

    fix: resolve layout issues on the homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,48 +3,45 @@
 </svelte:head>
 
 <main>
-    <div class="relative h-[82vh] text-gray-900 sm:h-[70vh] md:h-[82vh] dark:text-gray-200">
-        <div class="flex flex-col items-center justify-between md:flex-row">
+    <div class="relative max-w-screen mx-4 mt-20 md:mx-32 md:mt-10 text-gray-900 dark:text-gray-200">
+        <div class="flex flex-col items-center justify-between md:flex-row h-[55vh] sm:h-[70vh] md:h-[82vh]">
+            
             <!-- Left Section -->
-            <div
-                class="relative z-10 flex h-[55vh] w-1/2 flex-col items-center justify-center px-4 text-center sm:h-[60vh] md:h-[82vh]"
-            >
-                <h1 class="mb-4 text-3xl font-bold leading-tight md:text-4xl">
+            <section class="relative z-10 flex flex-col items-center justify-center px-4 text-center md:w-1/2 w-2/3 sm:h-[60vh] md:h-[82vh]">
+                <h1 class="mb-4 text-xl font-bold leading-tight md:text-4xl">
                     Finding the class routine has always been a cumbersome task
                 </h1>
-                <p class="mb-8 text-2xl text-gray-800 dark:text-gray-300">But is it still?</p>
+                <p class="mb-8 text-lg md:text-2xl text-gray-800 dark:text-gray-300">
+                    But is it still?
+                </p>
                 <a
                     href="/crt"
-                    class="transform rounded-full bg-blue-600 px-6 py-2 text-lg font-semibold text-gray-100 transition duration-300 ease-in-out hover:scale-105 hover:bg-blue-500 hover:shadow-lg"
-                    aria-label="Get Started with the class routine"
+                    class="transform rounded-full bg-blue-600 px-6 py-2 text-sm md:text-lg font-semibold text-gray-100 transition duration-300 ease-in-out hover:scale-105 hover:bg-blue-500 hover:shadow-lg"
+                    aria-label="Find the class routine"
                 >
                     Find class routine
                 </a>
-            </div>
+            </section>
 
             <!-- Slim Line (Border) -->
-            <div
-                class="mx-4 my-4 h-full w-64 border-t-2 border-gray-300 sm:h-[55vh] md:my-0 md:h-[65vh] md:w-[2px] md:border-l-2"
-            ></div>
+            <hr class="mx-4 my-8 w-64 sm:h-[55vh] md:my-0 md:h-[65vh] md:w-[2px] border-t-2 md:border-l-2 border-gray-300" />
 
             <!-- Right Section -->
-            <div
-                class="relative z-10 flex h-[55vh] w-1/2 flex-col items-center justify-center px-4 text-center sm:h-[60vh] md:h-[82vh]"
-            >
-                <h1 class="mb-4 text-3xl font-bold leading-tight md:text-4xl">
+            <section class="relative z-10 flex flex-col items-center justify-center px-4 text-center md:w-1/2 w-2/3 sm:h-[60vh] md:h-[82vh]">
+                <h1 class="mb-4 text-xl font-bold leading-tight md:text-4xl">
                     Confused about choosing a retake section?
                 </h1>
-                <p class="mb-8 text-2xl text-gray-800 dark:text-gray-300">
+                <p class="mb-8 text-lg md:text-2xl text-gray-800 dark:text-gray-300">
                     Say goodbye to confusion
                 </p>
                 <a
                     href="/rsrt"
-                    class="transform rounded-full bg-blue-600 px-6 py-2 text-lg font-semibold text-gray-100 transition duration-300 ease-in-out hover:scale-105 hover:bg-blue-500 hover:shadow-lg"
-                    aria-label="Get Started with the class routine"
+                    class="transform rounded-full bg-blue-600 px-6 py-2 text-sm md:text-lg font-semibold text-gray-100 transition duration-300 ease-in-out hover:scale-105 hover:bg-blue-500 hover:shadow-lg"
+                    aria-label="Find the retake section"
                 >
                     Find retake section
                 </a>
-            </div>
+            </section>
         </div>
     </div>
 </main>
