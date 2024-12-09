d61aa722eea3620285b7312e776d798dbb75bb23
kurtnettle <89929240+KurtNettle@users.noreply.github.com>
2024-12-10 03:32:45 +0600

    style: improve homepage CSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,44 +3,61 @@
 </svelte:head>
 
 <main>
-    <div class="relative max-w-screen mx-4 mt-20 md:mx-32 md:mt-10 text-gray-900 dark:text-gray-200">
-        <div class="flex flex-col items-center justify-between md:flex-row h-[55vh] sm:h-[70vh] md:h-[82vh]">
-            
+    <div class="relative mx-auto mt-10 max-w-5xl text-gray-900 md:mt-32 dark:text-gray-200">
+        <div class="flex flex-col items-center justify-between md:flex-row">
             <!-- Left Section -->
-            <section class="relative z-10 flex flex-col items-center justify-center px-4 text-center md:w-1/2 w-2/3 sm:h-[60vh] md:h-[82vh]">
-                <h1 class="mb-4 text-xl font-bold leading-tight md:text-4xl">
-                    Finding the class routine has always been a cumbersome task
-                </h1>
-                <p class="mb-8 text-lg md:text-2xl text-gray-800 dark:text-gray-300">
-                    But is it still?
-                </p>
-                <a
-                    href="/crt"
-                    class="transform rounded-full bg-blue-600 px-6 py-2 text-sm md:text-lg font-semibold text-gray-100 transition duration-300 ease-in-out hover:scale-105 hover:bg-blue-500 hover:shadow-lg"
-                    aria-label="Find the class routine"
+            <section
+                class="relative z-10 flex w-2/3 flex-col items-center justify-center px-4 text-center md:w-1/2"
+            >
+                <div
+                    class="flex h-full items-center justify-center rounded-xl border bg-gray-100 p-4 shadow-md transition duration-300 hover:scale-105 hover:shadow-xl md:p-6 dark:border-zinc-800 dark:bg-zinc-900"
                 >
-                    Find class routine
-                </a>
+                    <div>
+                        <h1 class="mb-4 text-xl font-bold leading-tight md:text-3xl">
+                            Finding the class routine has always been a cumbersome task
+                        </h1>
+                        <p class="mb-8 text-lg text-gray-800 md:text-2xl dark:text-gray-300">
+                            But is it still?
+                        </p>
+                        <a
+                            href="/crt"
+                            class="transform rounded-full bg-blue-600 px-2 py-2 text-sm font-semibold text-gray-100 transition duration-300 ease-in-out hover:bg-blue-500 hover:shadow-lg sm:text-base md:px-6 md:text-lg"
+                            aria-label="Find the class routine"
+                        >
+                            Find class routine
+                        </a>
+                    </div>
+                </div>
             </section>
 
             <!-- Slim Line (Border) -->
-            <hr class="mx-4 my-8 w-64 sm:h-[55vh] md:my-0 md:h-[65vh] md:w-[2px] border-t-2 md:border-l-2 border-gray-300" />
+            <hr
+                class="mx-4 my-8 h-0 w-64 border-t-2 border-gray-300 md:my-0 md:h-[55vh] md:w-[2px] md:border-l-2"
+            />
 
             <!-- Right Section -->
-            <section class="relative z-10 flex flex-col items-center justify-center px-4 text-center md:w-1/2 w-2/3 sm:h-[60vh] md:h-[82vh]">
-                <h1 class="mb-4 text-xl font-bold leading-tight md:text-4xl">
-                    Confused about choosing a retake section?
-                </h1>
-                <p class="mb-8 text-lg md:text-2xl text-gray-800 dark:text-gray-300">
-                    Say goodbye to confusion
-                </p>
-                <a
-                    href="/rsrt"
-                    class="transform rounded-full bg-blue-600 px-6 py-2 text-sm md:text-lg font-semibold text-gray-100 transition duration-300 ease-in-out hover:scale-105 hover:bg-blue-500 hover:shadow-lg"
-                    aria-label="Find the retake section"
+            <section
+                class="relative z-10 flex w-2/3 flex-col items-center justify-center px-4 text-center md:w-1/2"
+            >
+                <div
+                    class="flex h-full items-center justify-center rounded-xl border bg-gray-100 p-4 shadow-md transition duration-300 hover:scale-105 hover:shadow-xl md:p-6 dark:border-zinc-800 dark:bg-zinc-900"
                 >
-                    Find retake section
-                </a>
+                    <div>
+                        <h1 class="mb-4 text-xl font-bold leading-tight md:text-3xl">
+                            Confused about choosing a retake section?
+                        </h1>
+                        <p class="mb-8 text-lg text-gray-800 md:text-2xl dark:text-gray-300">
+                            Say goodbye to confusion
+                        </p>
+                        <a
+                            href="/rsrt"
+                            class="transform rounded-full bg-blue-600 px-2 py-2 text-sm font-semibold text-gray-100 transition duration-300 ease-in-out hover:bg-blue-500 hover:shadow-lg sm:text-base md:px-6 md:text-lg"
+                            aria-label="Find the retake section"
+                        >
+                            Find retake section
+                        </a>
+                    </div>
+                </div>
             </section>
         </div>
     </div>
